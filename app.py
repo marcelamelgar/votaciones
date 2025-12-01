892b77d4d99f8fce8b2e31d44c9141dfbda02bf1
Marcela Melgar <61280126+marcelamelgar@users.noreply.github.com>
2025-12-01 11:25:42 -0600

    EN CONTRA A FAVOR

--- app.py
+++ app.py
@@ -400,7 +400,7 @@ if seccion.startswith("6433"):
     with res1:
         st.markdown(f"""
         <div class="metric-card">
-            <div class="metric-title">A FAVOR del CACIF (2ª)</div>
+            <div class="metric-title">EN CONTRA del CACIF (2ª)</div>
             <div class="metric-value">{favor_2}</div>
         </div>
         """, unsafe_allow_html=True)
@@ -408,7 +408,7 @@ if seccion.startswith("6433"):
     with res2:
         st.markdown(f"""
         <div class="metric-card">
-            <div class="metric-title">EN CONTRA del CACIF (2ª)</div>
+            <div class="metric-title">A FAVOR del CACIF (2ª)</div>
             <div class="metric-value">{contra_2}</div>
         </div>
         """, unsafe_allow_html=True)
